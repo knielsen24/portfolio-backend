69928f7fdb10370297f0bf4ccabc7cec7a6ea768
kevinnielsen <knielsen24@gmail.com>
2024-02-09 19:25:21 -0800

    added express json middleware

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ config();
 
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-
+import cors from "cors";
 import Project from "./models/Project";
 
 const app = express();
+
+app.use(cors())
+app.use(express.json())
+
 const PORT = 8000;
 
 let dbURI = process.env.MONGO_URL;
 
 app.post("/projects", async (req: Request, res: Response) => {
-
   const newProject = new Project({
     title: "triple stamp",
   });
